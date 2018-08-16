0eb94208641956eaf74f13f01b79019416664689
Ryan Gibb <ryangibb321@gmail.com>
2018-08-16 15:43:19 +0100

    Moved start and end state, and getNextState methods to State.

--- src/MarkovChain.java
+++ src/MarkovChain.java
@@ -2,8 +2,6 @@ import java.util.*;
 
 public class MarkovChain<T> {
     private Set<State<T>> states = new HashSet<>();
-    private Set<State<T>> startStates = new HashSet<>();
-    private Set<State<T>> endStates = new HashSet<>();
     private Random random = new Random();
 
     // Checks if State with a given value exists, adds it to the set states if not, and returns the State
@@ -26,34 +24,6 @@ public class MarkovChain<T> {
         state.addOccurrence(nextState);
     }
 
-    public void addStartState(T stateValue){
-        startStates.add(getStateWithValue(stateValue));
-    }
-
-    public void addEndState(T stateValue){
-        endStates.add(getStateWithValue(stateValue));
-    }
-
-    public State getNextState(State<?> state) {
-        Map<State, Integer> nextStateOccurrences = state.getNextStateProbabilities();
-        if (nextStateOccurrences.size() == 0){
-            return getRandomState();
-        }
-        int totalOccurrences = nextStateOccurrences.values().parallelStream().reduce((a, b) -> a + b).orElse(0);
-        double randomInt = random.nextInt(totalOccurrences) + 1;
-        int sum = 0;
-        State nextState = null;
-        for (Map.Entry<State, Integer> entry : nextStateOccurrences.entrySet()) {
-            int occurrences = entry.getValue();
-            nextState = entry.getKey();
-            sum += occurrences;
-            if (sum >= randomInt){
-                break;
-            }
-        }
-        return nextState;
-    }
-
     private State getRandomState(Set<State<T>> statesSet) {
         List<State<T>> statesList = new ArrayList<>(statesSet);
         return statesList.get(random.nextInt(states.size()));
@@ -63,28 +33,10 @@ public class MarkovChain<T> {
         return getRandomState(states);
     }
 
-    public State getStartState(){
-        return getRandomState(startStates);
-    }
-
-    public State getEndState(){
-        return getRandomState(endStates);
-    }
-
-    public boolean isEndState(State state){
-        return endStates.contains(state);
-    }
-
-    public boolean isStartState(State state){
-        return startStates.contains(state);
-    }
-
     @Override
     public String toString() {
         return "MarkovChain{" +
                 "states=" + states +
-                ", startStates=" + startStates +
-                ", endStates=" + endStates +
                 '}';
     }
 }
--- src/State.java
+++ src/State.java
@@ -1,24 +1,42 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class State<T> {
     private T value;
-    private Map<State, Integer> nextStateProbabilities = new HashMap<>();
+    private Map<State, Integer> nextStateOccurrences = new HashMap<>();
+    private boolean startState = false;
+    private boolean endState = false;
 
     public State(T value) {
         this.value = value;
     }
 
     public void addOccurrence(State nextState){
-        nextStateProbabilities.merge(nextState, 1, (a, b) -> a + b);
+        nextStateOccurrences.merge(nextState, 1, (a, b) -> a + b);
     }
 
     public T getValue() {
         return value;
     }
 
-    public Map<State, Integer> getNextStateProbabilities() {
-        return nextStateProbabilities;
+    public State getNextState(Random random) {
+        if (nextStateOccurrences.size() == 0){
+            return null;
+        }
+        int totalOccurrences = nextStateOccurrences.values().parallelStream().reduce((a, b) -> a + b).orElse(0);
+        double randomInt = random.nextInt(totalOccurrences) + 1;
+        int sum = 0;
+        State nextState = null;
+        for (Map.Entry<State, Integer> entry : nextStateOccurrences.entrySet()) {
+            int occurrences = entry.getValue();
+            nextState = entry.getKey();
+            sum += occurrences;
+            if (sum >= randomInt){
+                break;
+            }
+        }
+        return nextState;
     }
 
     @Override
@@ -30,4 +48,29 @@ public class State<T> {
         return super.equals(o);
     }
 
+    public boolean isStartState() {
+        return startState;
+    }
+
+    public void setStartState(boolean startState) {
+        this.startState = startState;
+    }
+
+    public boolean isEndState() {
+        return endState;
+    }
+
+    public void setEndState(boolean endState) {
+        this.endState = endState;
+    }
+
+    @Override
+    public String toString() {
+        return "State{" +
+                "value=" + value +
+                ", nextStateOccurrences=" + nextStateOccurrences +
+                ", startState=" + startState +
+                ", endState=" + endState +
+                '}';
+    }
 }
